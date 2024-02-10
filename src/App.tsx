49f04ae45cc1a28241833c4d60d76983311b67d9
Dmitry <dmitry27zh@gmail.com>
2024-02-10 16:42:10 +0500

    Add App template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Button, Container, Stack } from 'react-bootstrap'
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <Container className="my-4">
+      <Stack className="mb-4" direction="horizontal" gap={2}>
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="outline-primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+          gap: '1rem',
+          alignItems: 'flex-start',
+        }}
+      >
+        <BudgetCard />
+      </div>
+    </Container>
+  )
 }
 
 export default App
